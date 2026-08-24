a30425e43b84f6be81dfdf0b9c448b747f71da51
AwAQwQAwAQwQAwA <3441424487@qq.com>
2026-08-25 02:45:31 +0800

    Fix the sort bug caused by Category

--- src/main/java/dev/budao/sortscroller/SortScroller.java
+++ src/main/java/dev/budao/sortscroller/SortScroller.java
@@ -1,6 +1,7 @@
 package dev.budao.sortscroller;
 
-import dev.budao.sortscroller.config.SortConfigs;
+import dev.budao.sortscroller.utils.SortingCategory;
+import dev.budao.sortscroller.utils.SortingMethod;
 import fi.dy.masa.itemscroller.ItemScroller;
 import fi.dy.masa.itemscroller.gui.GuiConfigs;
 import net.minecraftforge.fml.common.Mod;
@@ -13,15 +14,15 @@ public class SortScroller {
         if (FMLLoader.getDist().isClient()) {
             // Make sure the mod being absent on the other network side does not cause
             // the client to display the server as incompatible
-//            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
-//            ItemScroller.onInitialize();
-//
-//            // Config Screen
-//            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
-//                GuiConfigs gui = new GuiConfigs();
-//                gui.setParent(screen);
-//                return gui;
-//            });
+            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
+            SortingCategory.INSTANCE.buildItemGroupList();
+
+            // Config Screen
+            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
+                GuiConfigs gui = new GuiConfigs();
+                gui.setParent(screen);
+                return gui;
+            });
         }
     }
 }
